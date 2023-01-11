ca709f1896cca80ab1a8d765c4cfaefdc9cb41fc
natural-nine <wan.jun.bruce@gmail.com>
2023-01-11 14:16:20 +0900

    initial settings

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Home = () => {
+  return <HomeBox>hh</HomeBox>;
+};
+
+const HomeBox = styled.div`
+  background-color: red;
+`;
+
+export default Home;
